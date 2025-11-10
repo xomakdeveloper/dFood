d365e6ef1ab8d5d8745024c0efbe9b0f4743892a
xomakdev <xomakdevelopment@gmail.com>
2025-11-10 23:45:43 +0200

    1main

---
+++ src/main/java/xyz/xomakdev/dFood/dFood.java
@@ -0,0 +1,17 @@
+package xyz.xomakdev.dFood;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class dFood extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
